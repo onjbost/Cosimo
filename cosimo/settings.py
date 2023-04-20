0ecd7118d001b90d4c9b56f067cc4fcaa8550eab
onjbost <76117978+onjbost@users.noreply.github.com>
2023-04-20 12:30:50 +0200

    Update settings.py

--- cosimo/settings.py
+++ cosimo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eg90%0lqu$gma!wfmw@-cwj$y3+ell%b-s0kzz_=3o5h5c2m(8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
